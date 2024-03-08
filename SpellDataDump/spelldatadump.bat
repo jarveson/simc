8f15b34c85a70de8b95a93ee553f643d8d3e56cf
jarves <jarveson@gmail.com>
2024-03-08 17:23:20 -0600

    ﻿spell data dump
    Signed-off-by: jarves <jarveson@gmail.com>

--- SpellDataDump/spelldatadump.bat
+++ SpellDataDump/spelldatadump.bat
@@ -2,9 +2,9 @@ cd ..
 set classes=warrior,hunter,monk,paladin,rogue,shaman,mage,warlock,druid,deathknight,priest,demonhunter,evoker
 
 for %%i in (%classes%) do (
-simc display_build="0" spell_query="spell.class=%%i">spelldatadump/%%i.txt
+E:\gitrepos\simc\out\build\x64-Debug\simc display_build="0" spell_query="spell.class=%%i">spelldatadump/%%i.txt
 )
 
-simc display_build="0" spell_query="spell">spelldatadump/allspells.txt
+E:\gitrepos\simc\out\build\x64-Debug\simc display_build="0" spell_query="spell">spelldatadump/allspells.txt
 
-simc display_build="2">spelldatadump/build_info.txt
+E:\gitrepos\simc\out\build\x64-Debug\simc display_build="2">spelldatadump/build_info.txt
